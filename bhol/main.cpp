f486986f2889db4d13d23034809b0faf1f822e4e
seen <seen@xds.hu>
2025-02-16 19:38:19 +0100

    NUKE: intermediate debug version

--- bhol/main.cpp
+++ bhol/main.cpp
@@ -1,10 +1,13 @@
 #include <chrono>
 #include <cmath>
+#include <iostream>
 #include <list>
 #include <ncurses.h>
+#include <numbers>
 #include <random>
 #include <ranges>
 #include <thread>
+#include <tuple>
 
 using namespace std;
 using namespace chrono_literals;
@@ -15,183 +18,361 @@ struct Random {
 	uniform_int_distribution<> dist;
 	Random() : rng(this->dev()), dist(0, 10000) {}
 
-	double get(size_t m) {return this->dist(rng) % m;}
+	auto get(size_t m) {return this->dist(rng) % m;}
 };
 
-struct Vector {
-	double x, y;
+struct ScreenPosition {
+    int x;
+    int y;
 
-	void add(Vector const& other) {
-		this->x += other.x;
-		this->y += other.y;
+    auto operator<=> (ScreenPosition const&) const = default;
+};
+
+struct Window {
+    using ncurses_window = unique_ptr<WINDOW, decltype([](auto w){delwin(w);})>;
+    ncurses_window w;
+    Window(size_t x, size_t y, size_t px = 0, size_t py = 0) : w(newwin(y, x, py, px)) {
+        nodelay(this->w.get(), 1);
+    }
+
+    auto get() const {
+        return this->w.get();
+    }
+
+    auto refresh() {
+		wrefresh(get());
 	}
-    void sub(Vector const& other) {
-        this->x -= other.x;
-        this->y -= other.y;
+
+    auto get_max() const {
+        struct {int xmax, ymax;} r;
+        getmaxyx(get(), r.ymax, r.xmax);
+        return r;
     }
-    void scale(double scalar) {
-        this->x *= scalar;
-        this->y *= scalar;
+    
+    auto is_in(ScreenPosition const& p) const {
+        if(p.x < 0 || p.y < 0)
+            return false;
+        auto [xmax, ymax] = get_max();
+        if(p.x >= xmax || p.y >= ymax)
+            return false;
+        return true;
     }
-	void average(Vector const& other){
-		add(other);
-		this->x /= 2;
-		this->y /= 2;
+
+    auto put(int x, int y, char ch) {
+        mvwaddch(get(), y, x, ch);
+    }
+    auto put(int x, int y, string s) {
+        mvwaddstr(get(), y, x, s.c_str());
+    }
+
+    auto bold(bool b) {
+        b ? wattron(get(), A_BOLD) : wattroff(get(), A_BOLD);
+    }
+    
+    auto clear() {
+    	wclear(get());
+    }
+
+    auto key_pressed() const {
+        int c = wgetch(get());
+        return (c != ERR);
+    }
+
+    auto wait_for_key() {
+        nodelay(get(), 0);
+        int c = wgetch(get());
+        nodelay(get(), 1);
+        return c;
+    }
+};
+
+struct Screen {
+	int xmax, ymax = 0;
+    Screen() {
+        initscr();
+        curs_set(0);
+	    getmaxyx(stdscr, this->ymax, this->xmax);
+    }
+    ~Screen() {
+        endwin();
+    }
+
+	auto get_window(size_t x, size_t y, size_t px = 0, size_t py = 0) {
+        return Window(x, y, px, py);
 	}
-    void length(double nl) {
-        auto l = length();
-        scale(nl/l);
-    }
-    void max(double l) {
-        if(length() > l)
-            length(l);
-    }
-    double distance(Vector const& other){
-        return Vector(this->x-other.x, this->y-other.y).length(); 
-    }
-    double angle() const {
-        return std::atan2(x, y);
-    }
-    double length() const {
-        return std::sqrt(x*x + y*y);
-    }
-    Vector normalize() const {
-        const auto l = length();
-        return Vector(x/l, y/l);
-    }
 };
 
 // ---------------------------------------------------------------------- //
 
+struct Vector {
+	double x, y;
+
+    // adds another vector
+	auto add(Vector const& other) {
+		this->x += other.x;
+		this->y += other.y;
+	}
+    // subtracts another vector
+    auto sub(Vector const& other) {
+        this->x -= other.x;
+        this->y -= other.y;
+    }
+    // scales the vector with a scalar
+    auto scale(double scalar) {
+        this->x *= scalar;
+        this->y *= scalar;
+    }
+    // scales the vector to the passed in magnitude
+    auto magnitude(double nl) {
+        auto l = magnitude();
+        scale(nl/l);
+    }
+    // limits the magnitude of the vector
+    auto limit(double l) {
+        if(magnitude() > l)
+            magnitude(l);
+    }
+    // creates a vector based on an angle in radians
+    auto angle(double a) {
+        this->y = 1;
+        this->x = 1/tan(a);
+    }
+    // calculates the distance of two vectors
+    auto distance(Vector const& other) {
+        return Vector(this->x-other.x, this->y-other.y).magnitude(); 
+    }
+    // returns the angle in radians
+    auto angle() const -> double {
+        return atan2(y, x);
+    }
+    // returns the magnitude (length) of a vector
+    auto magnitude() const -> double {
+        // Pythaghoras
+        return sqrt(x*x + y*y);
+    }
+    // returns the normalized version of this vector (with magnitude 1)
+    auto normalize() const {
+        const auto l = magnitude();
+        return Vector(x/l, y/l);
+    }
+
+    auto to_screen(double scale) -> ScreenPosition const {
+        return {int(this->x*scale), int(this->y*scale)};
+    }
+
+    static auto from_screen(ScreenPosition const& sp, double scale) -> Vector {
+        // make the calculation in double
+        double x = sp.x, y = sp.y;
+        return {x/scale, y/scale};
+    }
+};
+
+// calculates the difference of two vectors
+auto difference(Vector const& v0, Vector const& v1) {
+    Vector v = v0;
+    v.sub(v1);
+    return v;
+}
+
+// ---------------------------------------------------------------------- //
+
 namespace Constants {
-    const double G = 6.67E-11; // gravitational constant
-    const double c = 2.99792458; // speed of light (scaled)
-    const double time_factor = .1; // how fast particles are travelling
-    const double blackhole_mass = 500000000000;//E11;
-    const Vector particle_velocity = {0, 0}; // initial velocity
+    // gravitational constant (https://en.wikipedia.org/wiki/Gravitational_constant)
+    const double G = 6.6743E-11;
+    // m/s speed of light in vacuum
+    const double c = 299792458;
+
+    // M (solar mass) this determines the size and force of the black hole Sagittarius A* (https://en.wikipedia.org/wiki/Sagittarius_A*)
+    const double blackhole_mass = 4.297E6;
+    const double solar_mass = 1.989E30;
+
+    // this determines the frame rate of the movement
+    const double time_factor = 1;
+    // scaling of the universe
+    const double scale = 1E-9;
+    // initial direction of the particles
+    const Vector particle_velocity = {-1, 0};
+    // how long the trail is for the particles
     const size_t history_size = 10;
 }
 
 struct Particle {
+    // the position of the particle in our geometry (https://en.wikipedia.org/wiki/2D_computer_graphics)
 	Vector	p;
+    // speed of the particle (velocity)
 	Vector  v;
+    // particles have zero mass, but for simplicity we will work with 1
     double  mass = 1;
+    // stores information if the particle needs to be calculated or is already eaten by the blackhole
 	bool	show = true;
-    std::list<std::pair<std::size_t, std::size_t>> history;
+    // stores the old screen positions of the particle to be able to draw the "trail"
+    // it stores the position in integers not in double as its easier to check for equality and at the
+    // end the screen positions are integers
+    list<ScreenPosition> history;
 
-    void move() {
-        std::pair<std::size_t, std::size_t> pp = {this->p.x, this->p.y};
-        if(this->history.empty() || 
-            this->history.front() != pp)
-                this->history.push_front(pp);
+
+    Vector rv;
+    double last_r;
+    double last_f;
+    Vector last_v;
+
+    auto move(Vector const& f) {
+        ScreenPosition last = p.to_screen(Constants::scale);
+        // only store the last position if it is different from the last one stored
+        // this ensures that even if the particle ramained in the same position, it will have a trail
+        if(this->history.empty() || this->history.front() != last)
+                this->history.push_front(last);
+        // limit the history to the trail size
         if(this->history.size() > Constants::history_size)
             this->history.resize(Constants::history_size);
+
+        last_v = this->v;
+        this->v.add(f);
+        // ensure that the particle always moves with the speed of light
+        this->v.magnitude(Constants::c);
+        Vector pv = this->v;
+        // scale it to our time frame
+        pv.scale(Constants::time_factor);
         // move the particle (add velocity to position)
-        this->p.add(this->v);     
+        rv = pv;
+        this->p.add(pv);     
     }
 
-	void draw(WINDOW *w, Vector bh){
-        if(this->p.x < 0 || this->p.y < 0)
-            return;
- 		if(!this->show)
-			return;
-        if(this->p.distance(bh) < 5)
+	auto draw(Window &w) {
+        auto sp = this->p.to_screen(Constants::scale);
+        w.put(0,  9, "vlast: " + to_string(last_v.x) + ":" + to_string(last_v.y) + ", mag: " + to_string(last_v.magnitude()));
+        w.put(0, 10, "v: " + to_string(rv.x) + ":" + to_string(rv.y) + ", mag: " + to_string(rv.magnitude()));
+        w.put(0, 11, "p: " + to_string(p.x) + ":" + to_string(p.y) + ", distance: " + to_string(last_r));
+        w.put(0, 12, "d: " + to_string(last_r));
+        w.put(0, 13, "f: " + to_string(last_f));
+        w.put(0, 14, "p: " + to_string(sp.x) + ":" + to_string(sp.y) + ", show: " + to_string(show));
+
+        if(!this->show || !w.is_in(sp))
             return;
         static string_view berill = "BERILL";
+        // we start from 1 as the first character will always be printed even when there is no tail
         std::size_t i = 1;
         for(auto const& p: history) {
-            mvwaddch(w, p.second, p.first, i < berill.length() ? berill[i] : '.');
+            w.put(p.x, p.y, i < berill.length() ? berill[i] : '.');
             ++i;
         }
-        wattron(w, A_BOLD);
-		mvwaddch(w, this->p.y, this->p.x, berill[0]);
-        wattroff(w, A_BOLD);
+        w.put(0, 1, "angle: " + to_string(this->v.angle()) + ", v.angle: " + to_string(this->v.angle()));
+        w.bold(true);
+		w.put(sp.x, sp.y, berill[0]);
+        w.bold(false);
 	}
 };
 
 
 struct Blackhole {
 	Vector p;
-    double mass;
-    const double event_horizon = 2*Constants::G*mass/(Constants::c*Constants::c);
+    double mass; // in solar mass
+    // calculate the Schwarzschild radius (event horizon) of the blackhole (https://en.wikipedia.org/wiki/Schwarzschild_radius)
+    // r = 2 × G × M/c*c
+    const double r = 2*Constants::G*mass*Constants::solar_mass/(Constants::c*Constants::c);
 
-    void draw(WINDOW *w) {
-        if(this->p.x < 0 || this->p.y < 0)
+    auto draw(Window &w) {
+        auto sp = this->p.to_screen(Constants::scale);
+        if(!w.is_in(sp))
             return;
-        mvwaddch(w, this->p.y, this->p.x, ' ');
+
+        const int R = this->r*Constants::scale;
+        // calculate the bounding square for the circle, add 1 to make sure the whole circle is included
+        // as there might be rounding mistakes
+        const int N = 2*R+1;
+
+        // exit if we have set a weird large universe
+        auto [xmax, ymax] = w.get_max();
+        if(N>min(xmax, ymax)) {
+            cout << "Blackhole too big\n";
+            exit(-1);
+        }
+
+        // simple filled circle raster algorithm, which goes through each
+        // pixed in the bounding box of the circle and fills it, it its inside
+        // the circle (distance is smaller than radius)
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                int x = i-R;
+                int y = j-R;
+
+                // Pythagoras to see if the point is inside the triangle to the rim of the circle
+                if (x*x + y*y <= R*R+1)
+                    w.put(sp.x-N/2+i, sp.y-N/2+j, 'o');
+            }
+        }
+    }
+    auto newtonian_pull(Particle &p) {
+        // calculate the distance vector between the two center points
+        // the magnitude
+        Vector towards_blackhole = difference(this->p, p.p);
+        // the distance from the particle to the blackhole is the length of the vector
+        // Newton's law of universal gravity (https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation)
+        //    F = G*m0*m1/(r*r)
+        // where F the gravitaion force, G is the gravitation constant, m0-m1 are the masses of the two objects, r is the distance between m0 and m1
+        auto r = towards_blackhole.magnitude();
+        auto F = Constants::G*this->mass*Constants::solar_mass*p.mass/(r*r);
+        p.last_f = F;
+        p.last_r = r*r;
+        // we already have the direction of the force from the vector, now we just need to limit its force to F by setting its length
+        towards_blackhole.magnitude(F);
+        return towards_blackhole;
     }
 
-	void pull(Particle &p){
-        // calculate distance vector
+	auto proper_pull(Particle &p){
+        /*// calculate distance vector
 		Vector dv = this->p;
         dv.sub(p.p);
-		double distance = dv.length();
+        auto angle = dv.angle();
+		double distance = dv.magnitude();
         double gravity = Constants::G*(this->mass*p.mass)/(distance*distance);
 
-        // set to gravity
-        dv.length(gravity);
-        p.v.add(dv);
-        // travel with the speed of light
-        p.v.length(Constants::c * Constants::time_factor);
+        //double delta_angle = gravity*(Constants::time_factor/Constants::c) * std::sin(p.angle - angle);
+        //delta_angle /= std::abs(1.-2.*Constants::G*this->mass/(distance*Constants::c*Constants::c));
+        //p.angle += delta_angle;
+        //p.angle += std::sin(p.angle - angle);
 
-       if(distance < this->event_horizon)
-			p.show = false;
-	}
+        p.v.angle(angle);
+        p.v.magnitude(Constants::c);
+        
+        if(distance < this->r)
+            p.show = false;*/
+    }
 };
 
-
-struct Screen {
-	WINDOW *w = 0;
-	size_t x, y = 0;
-
-	void init() {
-		initscr();
-		curs_set(0);
-        start_color();
-        init_pair(1, COLOR_BLACK, COLOR_GREEN);
-		getmaxyx(stdscr, this->y, this->x);
-		this->w = newwin(y+1, x, 0, 0);
-	}
-
-	void refresh() {
-		wrefresh(this->w);
-	}
-};
-
-
+// Declare it static, so we can exit() and still restore the terminal states
+static Screen s;
 
 
 int main() {
-   	Screen s;
-	s.init();
 	Random r;
+    Window w = s.get_window(s.xmax, s.ymax);
+    auto [xm, ym] = w.get_max();
 
-	Blackhole b{{double(s.x/8), double(s.y/2)}, Constants::blackhole_mass};
+	Blackhole b{Vector::from_screen({s.xmax/8, s.ymax/2}, Constants::scale), Constants::blackhole_mass};
 
-    std::vector<Particle> ps;
-    ps.reserve(s.x*s.y);
-    mvaddstr(0, 0, string("screen size: " + std::to_string(s.x) + ":" + std::to_string(s.y)).c_str());
-    s.refresh();
+    vector<Particle> ps;
 
-
-    const size_t height = s.y/2;
-    for(auto i = 0u; i < height; ++i) {
-        ps.push_back({{double(s.x-r.get(10)), double(i)}, Constants::particle_velocity});
+    const int height = s.ymax/2;
+    for(int i = 0; i < height; ++i) {
+        ps.push_back({Vector::from_screen({s.xmax-int(r.get(10)), i}, Constants::scale), Constants::particle_velocity});
     }
 
-
 	for(;;) {
-		wclear(s.w);
+        w.clear();
+        w.put(0, 0, "resolution: " + to_string(xm) + ":" + to_string(ym));
 		for(auto &p: ps) {
+            // do not even calculate dead particles
             if(!p.show)
                 continue;
-			b.pull(p);
-            p.move();
-			p.draw(s.w, b.p);
+			auto f = b.newtonian_pull(p);
+            p.move(f);
+			p.draw(w);
 		}
-        b.draw(s.w);
-		s.refresh();
-		std::this_thread::sleep_for(50ms);
+        b.draw(w);
+		w.refresh();
+        if(w.key_pressed())
+            w.wait_for_key();
+		this_thread::sleep_for(100ms);
 	}
-	endwin();
 }
